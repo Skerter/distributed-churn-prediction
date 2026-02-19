bbf4cb2961558aa12335b44a653b838269024e0b
skerter <geopank55@gmail.com>
2026-02-19 23:46:09 +0300

    косметика для единого формата main.py

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print('Hello, world!')
+import sys, dask, dask_kubernetes, dask_ml, xgboost, dask_xgboost, sklearn, pandas, numpy, pyarrow  # jupyter импорт не нужен, это метапакет
+print(f'Текущая версия Dask: {dask.__version__}')  # Должно вывести ~2026.2.x
+print(f'Текущая версия Python: {sys.version}')
+print("All imports successful!")
